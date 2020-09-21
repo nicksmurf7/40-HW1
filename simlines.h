2dbd1bb3fe5360eaaf588a1667c6a0dfaf823b39
nicksmurf7 <71105164+nicksmurf7@users.noreply.github.com>
2020-09-21 01:33:34 -0400

    Update simlines.h

--- simlines.h
+++ simlines.h
@@ -21,14 +21,22 @@ struct filedata {
     int identifier;
 };
 
-void compare_files(List_T allfiles, Table_T matches);
-int file_compare(struct filedata constantfile, struct filedata comparedfile, Table_T matches);
-int line_compare(char *line, char *secondaryline);
-void cleanline(char **line);
-int isacceptablecharacter(char c);
-int char_length(char *c);
-struct location makelocation(char *filename, int linenumber);
-void addtodata(int linecount, struct filedata file, char *line, Table_T matches);
-void print_table(const void *key, void **value, void *left);
-int check_repeat(Table_T matches, struct filedata file, char *aline, int linenumber);
-int filedata_compare(struct filedata file1, struct filedata file2);
+
+void run_Simlines(List_T allfiles, int argc, char *argv);
+void compare_Files(List_T allfiles, Table_T matches);
+void process_File(struct filedata file);
+int file_Compare(struct filedata constantfile, struct filedata comparedfile, Table_T matches);
+int line_Compare(char *line, char *secondaryline);
+void cleanLine(char **line);
+int isAcceptableChar(char c);
+int char_Length(char *c);
+struct location makeLocation(char *filename, int linenumber);
+void addToData(int linecount, struct filedata file, char *line, Table_T matches);
+void print_Table(const void *key, void **value, void *left);
+int check_Repeat(Table_T matches, struct filedata file, char *aline, int linenumber);
+int filedata_Compare(struct filedata file1, struct filedata file2);
+
+
+int string_Comp(char *str1, char *str2);
+void end_Simlines(Table_T matches);
+void remove_List(const void *key,void **value, void *cl);
