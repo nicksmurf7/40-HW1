e8d277ec745d8193e3ddce34b6e2fc901ea229a0
ReedK7 <71610447+ReedK7@users.noreply.github.com>
2020-09-22 11:47:36 -0400

    Update readaline.c

--- readaline.c
+++ readaline.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "readaline.h"
+void add_to_string(char **line, char c, int length);
+void reallocate(char **line, int malloc_size);
+
+#define INITIAL_SIZE 200
 
 size_t readaline(FILE *inputfd, char **datapp) {
-    int initialsize = 200;
-    char *line = malloc(initialsize);
-    
-    fgets(line, initialsize, inputfd);
-    
+    char *line;
+    int c;
+    int length = 0;
+    int malloc_size = INITIAL_SIZE;
+
+    line = (char *) malloc(INITIAL_SIZE);
+
+    c = fgetc(inputfd);
+
+    //TBC
+    if (feof(inputfd)) {
+                *datapp = NULL;
+                return 0;
+        }
+
+    while (!feof(inputfd)) {
+        add_to_string(&line, c, length);
+        length = length + 1;
+
+        if (length >= malloc_size) {
+            reallocate(&line, malloc_size);
+            malloc_size = malloc_size * 2;
+        }
+
+        if(c == 10) {
+            break;
+        }
+
+        c = fgetc(inputfd);
+    }
+
+    //printf("readaline read in:");
+    //printf("%s\n", line);
     *datapp = line;
-    return sizeof(line);
+    return length;
+}
+
+void add_to_string(char **line, char c, int length){
+    *(*line + length) = c;
+}
+void reallocate(char **line, int malloc_size) {
+    *line = realloc(*line, malloc_size * 2);
 }
