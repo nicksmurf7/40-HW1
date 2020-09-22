368be572bd1cad4cf4cc01c5ec353ba0af4ad6c3
ReedK7 <71610447+ReedK7@users.noreply.github.com>
2020-09-22 12:09:22 -0400

    Update simlines.h

--- simlines.h
+++ simlines.h
@@ -4,6 +4,7 @@
 #include <list.h>
 #include <table.h>
 #include <atom.h>
+#include <string.h>
 
 struct location {
     char *filename;
@@ -26,17 +27,18 @@ void run_Simlines(List_T allfiles, int argc, char *argv);
 void compare_Files(List_T allfiles, Table_T matches);
 void process_File(struct filedata file);
 int file_Compare(struct filedata constantfile, struct filedata comparedfile, Table_T matches);
-int line_Compare(char *line, char *secondaryline);
-void cleanLine(char **line);
+int line_Compare(char **line, char **secondaryline, int length);
+char *cleanLine(char **line, int length);
 int isAcceptableChar(char c);
 int char_Length(char *c);
 struct location makeLocation(char *filename, int linenumber);
-void addToData(int linecount, struct filedata file, char *line, Table_T matches);
+void addToData(int linecount, struct filedata file, const char *aline, Table_T matches);
 void print_Table(const void *key, void **value, void *left);
-int check_Repeat(Table_T matches, struct filedata file, char *aline, int linenumber);
+int check_Repeat(Table_T matches, struct filedata file, const char *aline, int linenumber);
 int filedata_Compare(struct filedata file1, struct filedata file2);
-
+void applyFree(void **ptr, void *cl);
+void printvalue(void **ptr, void *cl);
 
 int string_Comp(char *str1, char *str2);
-void end_Simlines(Table_T matches);
+void end_Simlines(Table_T matches, List_T allfiles);
 void remove_List(const void *key,void **value, void *cl);
