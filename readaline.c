6482b5dccc8ff665a062caa92c03388b66be1eb4
nicksmurf7 <71105164+nicksmurf7@users.noreply.github.com>
2020-09-22 19:09:34 -0400

    Update readaline.c

--- readaline.c
+++ readaline.c
@@ -1,3 +1,15 @@
+/*
+ * readaline.c
+ * by Nick Murphy (nmurph03) and Reed Kass-Mullet(rkassm01)
+ * assignment: Filesnpix, hw1
+ * 
+ * Contains readaline function and readaline helper functions
+ * Reads a line from user inputted file and modifies the user
+ * inputted char array (datapp) to contain the line that was
+ * read in
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "readaline.h"
@@ -6,6 +18,14 @@ void reallocate(char **line, int malloc_size);
 
 #define INITIAL_SIZE 200
 
+
+
+/* readaline
+ * Gets: User-inputted file, pointer to char array to hold line
+ * Returns: Size of line that was just read
+ * Does: Reads a line from inputfd, returns the size of 
+ * the read line
+ */
 size_t readaline(FILE *inputfd, char **datapp) 
 {
     char *line;
@@ -42,11 +62,21 @@ size_t readaline(FILE *inputfd, char **datapp)
     return length;
 }
 
+/* add_to_string
+ * Gets: Line to add char from, char to add, index of char
+ * Returns: Nothing
+ * Does: Adds char to string
+ */
 void add_to_string(char **line, char c, int length)
 {
     *(*line + length) = c;
 }
 
+/* reallocate
+ * Gets: Line that is too big, previously allocated size
+ * Returns: Nothing
+ * Does: Reallocs space for line when it is too long
+ */
 void reallocate(char **line, int malloc_size)
 {
     *line = realloc(*line, malloc_size * 2);
