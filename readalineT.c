85a70015c6df98353a4c955de5eaadebfa5cab81
ReedK7 <71610447+ReedK7@users.noreply.github.com>
2020-09-22 12:15:43 -0400

    Update readalineT.c

--- readalineT.c
+++ readalineT.c
@@ -1,3 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readaline.h"
+
+
+void add_to_string(char **line, char c, int length);
+void reallocate(char **line, int malloc_size);
+char *cleanLine(char **line, int length); 
+int isAcceptableChar(char c);
+
+#define INITIAL_SIZE 200
+
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
+    FILE *file = fopen("test.txt", "r");
+    FILE *Sfile = fopen("test2.txt", "r");
+    char *fline;
+    char *sline;
+    int x = readaline(file, &fline);
+    int y = readaline(Sfile,&sline);
+    printf("FLINE LENGTH == %d\n", x);
+    printf("SLINE LENGTH == %d\n", y);
+    printf("FLINE  == %s\n", fline);    
+    printf("SLINE  == %s\n", sline);  
+    char *fclean = cleanLine(&fline,x);
+    char *sclean = cleanLine(&sline,y);
+    printf("CLEAN FLINE == %s\n", fclean);
+    printf("CLEAN SLINE == %s\n", sclean);
+}
+
+
+size_t readaline(FILE *inputfd, char **datapp) {
+    char *line;
+    int c;
+    int length = 0;
+    int malloc_size = INITIAL_SIZE;
+
+    line = (char *) malloc(INITIAL_SIZE);
+
+    c = fgetc(inputfd);
+
+    while (!feof(inputfd)) {
+        add_to_string(&line, c, length);
+        length = length + 1;
+
+        if (length >= malloc_size) {
+            reallocate(&line, malloc_size);
+            malloc_size = malloc_size * 2;
+        }
+
+        if(c == 10) {
+            break;
+        }
+
+        c = fgetc(inputfd);
+    }
+
+    printf("readaline read in:");
+    printf("%s\n", line);
+    *datapp = line;
+    return length;
+}
+
+void add_to_string(char **line, char c, int length){
+    *(*line + length) = c;
+}
+void reallocate(char **line, int malloc_size) {
+    *line = realloc(*line, malloc_size * 2);
+}
+
+
 /* cleanLine
  * Gets: Line to be cleaned
  * Returns: Nothing
