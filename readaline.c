efd61182ca51329cf11beabd000da2527f27c801
ReedK7 <71610447+ReedK7@users.noreply.github.com>
2020-09-22 19:07:22 -0400

    Update readaline.c

--- readaline.c
+++ readaline.c
@@ -6,15 +6,8 @@ void reallocate(char **line, int malloc_size);
 
 #define INITIAL_SIZE 200
 
-
-
-/* readaline
- * Gets: User-inputted file, pointer to char array to hold line
- * Returns: Size of line that was just read
- * Does: Reads a line from inputfd, returns the size of 
- * the read line
- */
-size_t readaline(FILE *inputfd, char **datapp) {
+size_t readaline(FILE *inputfd, char **datapp) 
+{
     char *line;
     int c;
     int length = 0;
@@ -23,52 +16,38 @@ size_t readaline(FILE *inputfd, char **datapp) {
     line = malloc(INITIAL_SIZE);
 
     if (feof(inputfd)) {
-                *datapp = NULL;
-                free(line); //NEW
-                return 0;
+        *datapp = NULL;
+        free(line);
+        return 0;
     }
 
     c = fgetc(inputfd);
 
-    while (!feof(inputfd)) {
+    int end = 0;
+    
+    while (!feof(inputfd) && end == 0) {
         add_to_string(&line, c, length);
         length = length + 1;
-
         if (length >= malloc_size) {
             reallocate(&line, malloc_size);
             malloc_size = malloc_size * 2;
         }
-
         if(c == 10) {
-            break;
-        }
-
-        c = fgetc(inputfd);
+            end = 1;
+        } else {
+            c = fgetc(inputfd);
+        }   
     }
-
-    //printf("readaline read in:");
-    //printf("%s\n", line);
     *datapp = line;
     return length;
 }
 
-
-/* add_to_string
- * Gets: Line to add char from, char to add, index of char
- * Returns: Nothing
- * Does: Adds char to string
- */
-void add_to_string(char **line, char c, int length){
+void add_to_string(char **line, char c, int length)
+{
     *(*line + length) = c;
 }
 
-
-
-/* reallocate
- * Gets: Line that is too big, previously allocated size
- * Returns: Nothing
- * Does: Reallocs space for line when it is too long
- */
-void reallocate(char **line, int malloc_size) {
+void reallocate(char **line, int malloc_size)
+{
     *line = realloc(*line, malloc_size * 2);
 }
